4a2a6c092a84a3623ee55805258b7c256f5d639c
mansurissa <mansurissa6@gmail.com>
2020-07-20 17:35:38 +0200

    added: - install monoose - created POST router for employee - added test employee
    fixes #3

--- router/postRouter.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-
-const postRouter = express.Router();
-
-postRouter.route('/', 'post', (req, res) => {
-  res.json(console.log('it works'));
-});
-
-export default postRouter;
