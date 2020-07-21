acfe07d68ff60ab1ddb9b69d06f1e6c5101d9d96
mansurissa <mansurissa6@gmail.com>
2020-07-21 16:42:00 +0200

    adding and testing GET request on / route

--- router/employeeRouter.js
+++ router/employeeRouter.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import { employeePost } from '../controllers/employeePost';
+import { employeePost, employeesGet } from '../controllers/employeeController';
 
 const employeesRouter = express.Router();
 
-employeesRouter.route('/').post(employeePost);
+employeesRouter.route('/').post(employeePost).get(employeesGet);
+// employeesRouter.route('/:_id').delete(employeeDelete)
 
 export default employeesRouter;
