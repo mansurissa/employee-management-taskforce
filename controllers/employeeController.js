c101a26c0c73065f26bfe45aa2f44d2a95bd9c02
mansurissa <mansurissa6@gmail.com>
2020-07-21 16:41:13 +0200

    adding a controller for Getting all employees

---
+++ controllers/employeeController.js
@@ -0,0 +1,60 @@
+import mongoose from 'mongoose';
+import Employees from '../models/employeesModel';
+
+export const employeePost = async (req, res) => {
+  try {
+    const employee = await Employees.create({
+      name: req.body.name,
+      email: req.body.email,
+      phone: req.body.phone,
+      status: req.body.status,
+      position: req.body.position,
+      birth: req.body.birth,
+      nId: req.body.nId
+    });
+    res.status(201).json({
+      message: ' user created successfully',
+      employee
+    });
+  } catch (err) {
+    console.log('failed to write employee', err);
+    res.status(500).json({
+      error: err
+    });
+  }
+};
+
+export const employeeDelete = async (req, res) => {
+  try {
+  } catch (err) {
+    res.status(500).json({
+      message: 'User deleted successfully',
+      error: err
+    });
+  }
+};
+
+export const employeesGet = async (req, res) => {
+  try {
+    const employees = await Employees.find();
+    res.status(200).json({
+      message: ' employees fetched',
+      employees
+    });
+  } catch (error) {
+    console.log('Big Error: ', error);
+    res.status(500).json({
+      message: ' failed to fetch all employees',
+      error
+    });
+  }
+};
+
+// export const employeesGet = (req, res) => {
+//   Employees.find()
+//     .exec()
+//     .then(res.json('i am getting them'))
+//     .catch((err) => {
+//       res.json("i'm not getting them", err);
+//     });
+// };
