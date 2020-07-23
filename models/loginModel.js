e7a9f1aaf26fb0e24aed1a080920376b6279c1f7
mansurissa <mansurissa6@gmail.com>
2020-07-23 17:03:27 +0200

    model  of managers

---
+++ models/loginModel.js
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose';
+
+const loginSchema = mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+  },
+  password: { type: String, required: true },
+});
+export default mongoose.model('Managers', loginSchema);
