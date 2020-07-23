abf83cc826007e521d1ba9bfb8f79e47d9067733
mansurissa <mansurissa6@gmail.com>
2020-07-23 17:05:19 +0200

    managers of managers

---
+++ router/managersRouter.js
@@ -0,0 +1,8 @@
+import express from 'express';
+import { managersSignup } from '../controllers/managersController';
+
+const managersRouter = express.Router();
+
+managersRouter.route('/').post(managersSignup);
+
+export default managersRouter;
