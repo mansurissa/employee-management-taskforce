6255bb8548561eb317e54b985edd11d7ed3a82f5
mansurissa <mansurissa6@gmail.com>
2020-07-23 17:02:55 +0200

    controller of managers

---
+++ controllers/managersController.js
@@ -0,0 +1,22 @@
+import Managers from '../models/loginModel';
+
+export const managersSignup = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const manager = await Managers.create({
+      email,
+      password,
+    });
+    res.status(201).json({
+      success: true,
+      message: ' manager created',
+      manager,
+    });
+  } catch (error) {
+    res.status(201).json({
+      success: false,
+      message: ' manager was not created',
+      error,
+    });
+  }
+};
