8096ff7bf9b7f0c851cda05989af3430a3cf358b
mansurissa <mansurissa6@gmail.com>
2020-07-23 03:58:04 +0200

    suspending employee

--- router/employeeRouter.js
+++ router/employeeRouter.js
@@ -4,6 +4,6 @@ import { employeePost, employeesGet } from '../controllers/employeeController';
 const employeesRouter = express.Router();
 
 employeesRouter.route('/').post(employeePost).get(employeesGet);
-// employeesRouter.route('/:_id').delete(employeeDelete)
+employeesRouter.route('/:_id').delete(employeeDelete);
 
 export default employeesRouter;
